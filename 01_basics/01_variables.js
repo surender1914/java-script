4e725fea8555546cdf2210699a472cd4975efc0d
Surender Singh Ramgarhia <surenderramgarhia8tk@gmail.com>
2025-06-18 08:07:20 +0000

    a

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12344;
+let accountEmail = "surender@gmail.com"
+var accountPassword = "12345";
+accountCity = "raisinghnagar"
+
+// accountId = 244; //not allowed
+accountEmail = "su@gmail.com"
+accountPassword = "139832794"
+accountCity = "jaipur"
+
+console.table([accountEmail,accountPassword,accountCity,accountId])
+
+
+// prefer not to use var reason because of glob scope and block use 
