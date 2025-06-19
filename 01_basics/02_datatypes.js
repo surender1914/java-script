d3a744e6bfabf9b3bf18a9bd138ce5b265abd14a
Surender Singh Ramgarhia <surenderramgarhia8tk@gmail.com>
2025-06-19 03:18:39 +0000

    a

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+  
+//alert(3+3); we are using nodejs not browser
+
+// console.log("2">1)
+// console.log("02">1)
+// console.log(1==true)
+// console.log(0==false)
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
